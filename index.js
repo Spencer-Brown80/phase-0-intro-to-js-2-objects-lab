a8fb250166bf9f31c64bbd64a06058377d00ab85
Spencer <spinsab@gmail.com>
2024-02-04 19:00:20 -0500

    modifed objects

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution in this file!
+const employee = {}
+employee.name = `Sam`;
+employee.streetAddress = `11 Broadway`;
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+   const newEmployee = { ...employee};
+
+   newEmployee[key] = value;
+
+   return newEmployee;
+}
+
+const newEmployee = updateEmployeeWithKeyAndValue(
+      employee,
+      "streetAddress",
+      "12 Broadway"
+);
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee.streetAddress = "12 Broadway";
+
+     return employee;
+
+
+}
+
+function deleteFromEmployeeByKey(obj, key, value) {
+    const newEmployee1 = { ...employee};
+ 
+    newEmployee1[key] = value;
+    delete newEmployee1.streetAddress;
+ 
+    return newEmployee1;
+ }
+
+ function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    delete employee.name;
+
+    return employee;
+ }
+ 
+ 
